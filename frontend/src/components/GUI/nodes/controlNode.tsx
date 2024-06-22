1395162adfd457544254f165dd2b3ab67e1389c4
khokao <kokihokao@gmail.com>
2024-06-22 22:08:20 +0900

    update to fit with tauri system

--- frontend/src/components/GUI/nodes/controlNode.tsx
+++ frontend/src/components/GUI/nodes/controlNode.tsx
@@ -1,14 +1,27 @@
 import { onDragStart } from "@/components/GUI/Bottombar";
+import icon from "@public/images/node-control.svg";
 import Image from "next/image";
 import React from "react";
 
 const nodeType = "control";
 const iconUrl = "/images/node-control.svg";
-const width = 80;
-const height = 80;
+
+/*
+  Since dragging and dropping icons in Tauri apps can trigger file operations,
+  we disable the drag event for the image and enable it for the parent div instead.
+  To properly handle drag events via Tauri, set `tauri.windows.fileDropEnabled` to false in `tauri.conf.json`.
+*/
+const enableDragging = (event) => {
+  event.currentTarget.draggable = true;
+};
 
 export const controlNode = (
-  <div className="cursor-pointer" onDragStart={(event) => onDragStart(event, nodeType, iconUrl)} draggable>
-    <Image src={iconUrl} alt={nodeType} width={width} height={height} />
+  <div
+    className="cursor-pointer flex items-end justify-center"
+    onDragStart={(event) => onDragStart(event, nodeType, iconUrl)}
+    draggable
+    onMouseDown={enableDragging}
+  >
+    <Image src={icon} alt={nodeType} draggable={false} />
   </div>
 );
--- frontend/src/components/GUI/nodes/promoterNode.tsx
+++ frontend/src/components/GUI/nodes/promoterNode.tsx
@@ -1,14 +1,27 @@
 import { onDragStart } from "@/components/GUI/Bottombar";
+import icon from "@public/images/node-promoter.svg";
 import Image from "next/image";
 import React from "react";
 
 const nodeType = "promoter";
 const iconUrl = "/images/node-promoter.svg";
-const width = 80;
-const height = 80;
+
+/*
+  Since dragging and dropping icons in Tauri apps can trigger file operations,
+  we disable the drag event for the image and enable it for the parent div instead.
+  To properly handle drag events via Tauri, set `tauri.windows.fileDropEnabled` to false in `tauri.conf.json`.
+*/
+const enableDragging = (event) => {
+  event.currentTarget.draggable = true;
+};
 
 export const promoterNode = (
-  <div className="cursor-pointer" onDragStart={(event) => onDragStart(event, nodeType, iconUrl)} draggable>
-    <Image src={iconUrl} alt={nodeType} width={width} height={height} />
+  <div
+    className="cursor-pointer flex items-end justify-center"
+    onDragStart={(event) => onDragStart(event, nodeType, iconUrl)}
+    draggable
+    onMouseDown={enableDragging}
+  >
+    <Image src={icon} alt={nodeType} draggable={false} />
   </div>
 );
