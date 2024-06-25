5e341c370d7b769f8179bcd14ef56937e0b1479e
Thickstem <mosohaso3560@gmail.com>
2024-06-25 15:02:59 +0900

    npm package update

--- backend/simulator/api/routers.py
+++ backend/simulator/api/routers.py
@@ -1,4 +1,4 @@
-from typing import Dict, List
+from typing import Dict, List, Union
 
 import numpy as np
 from fastapi import APIRouter, Query
@@ -11,13 +11,15 @@ router = APIRouter()
 
 
 @router.post('/simulator', response_model=SimulatorOutput)
-async def run_simulation(input: SimulatorInput) -> SimulatorOutput:
+async def run_simulation(input: SimulatorInput) -> List[float]:
     simulator_result = [1.0, 2.0, 3.0, 4.0, 5.0]  # Dummy code. (TODO: Replace)
     return simulator_result  # Dummy code. (TODO: Replace)
 
 
 @router.get('/graph_interact', response_model=Dict[str, List[float]])
-async def get_data_param(param1: float = Query(1.0), param2: float = Query(1.0)) -> Dict[str, List[float]]:
+async def get_data_param(
+    param1: float = Query(1.0), param2: float = Query(1.0)
+) -> Dict[str, Union[List[float], List[str]]]:
     solution = run_euler_example(alpha1=param1, alpha2=param2)
     time = [str(t) for t in np.arange(0, len(solution)).tolist()]
     return {'data1': solution[:, 0].tolist(), 'data2': solution[:, 1].tolist(), 'time': time}
