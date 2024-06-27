3654aee6b2aeae25347805c29947580941aa77c9
Thickstem <mosohaso3560@gmail.com>
2024-06-27 18:46:52 +0900

    merge from simulator-chart

---
+++ frontend/src/hooks/useSimulatorAPI.ts
@@ -0,0 +1,9 @@
+import type { SimulatorResponseData, SimulatorRequestData } from "@/interfaces/simulatorAPI";
+import { invoke } from "@tauri-apps/api/tauri";
+
+export const callSimulatorAPI = async  (data: SimulatorRequestData) => {
+  return await invoke<SimulatorResponseData>("call_simulator_api", {
+    param1: data.param1,
+    param2: data.param2,
+  });
+};
