4ebcb1760b3eb656bc7fc41cda15a7063ce7e2a7
khokao <kokihokao@gmail.com>
2024-06-30 03:59:20 +0900

    add custom child node and parent node

--- frontend/src/components/GUI/CustomNode.tsx
+++ frontend/src/components/GUI/CustomNode.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Handle, Position } from "reactflow";
 
-export const CustomNode = ({ data }) => {
+export const CustomChildNode = ({ data }) => {
   return (
-    <div className="custom-node">
+    <div className="relative">
       <img src={data.iconUrl} alt={data.nodeType} className="" />
-      <Handle type="source" position={Position.Right} />
-      <Handle type="target" position={Position.Left} />
+      <Handle type="target" position={Position.Left} id="left" style={data.leftHandleStyle} />
+      <Handle type="source" position={Position.Right} id="right" style={data.rightHandleStyle} />
     </div>
   );
+};
+
+export const CustomParentNode = ({ data }) => {
+  return <div className="border-2 border-gray-500 rounded-xl" style={{ width: data.width, height: data.height }} />;
 };
