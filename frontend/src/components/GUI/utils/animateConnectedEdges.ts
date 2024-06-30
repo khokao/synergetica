36b84095ce54167d913bf4b487e0e88070feaa71
khokao <kokihokao@gmail.com>
2024-06-30 09:26:49 +0900

    add deactivate animation

--- frontend/src/components/GUI/utils/animateConnectedEdges.ts
+++ frontend/src/components/GUI/utils/animateConnectedEdges.ts
@@ -1,9 +1,21 @@
-export const animateConnectedEdges = (eds, nodeId) => {
+export const activateConnectedEdgesAnimation = (eds, nodeId) => {
   return eds.map((e) => {
     if (e.source === nodeId || e.target === nodeId) {
       return {
         ...e,
         animated: true,
+      };
+    }
+    return e;
+  });
+};
+
+export const deactivateConnectedEdgesAnimation = (eds, nodeId) => {
+  return eds.map((e) => {
+    if (e.source === nodeId || e.target === nodeId) {
+      return {
+        ...e,
+        animated: false,
       };
     }
     return e;
