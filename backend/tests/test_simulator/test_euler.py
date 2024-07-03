bf8fd5fff2d659aa581be078a42cfaef07422e96
Thickstem <mosohaso3560@gmail.com>
2024-07-03 17:30:53 +0900

    RM euler test func

--- backend/tests/test_simulator/test_euler.py
+++ backend/tests/test_simulator/test_euler.py
@@ -22,14 +22,6 @@ def test_euler(simple_function):
     assert result.dtype == np.float64
 
 
-def test_time_empty(simple_function):
-    times = np.array([])
-    var_init = [1.0]
-    args = (1.0, 1.0)
-    with pytest.raises(AssertionError):
-        euler(simple_function, times, var_init, args)
-
-
 def test_var_init_empty(simple_function):
     times = np.linspace(0, 10, 100)
     var_init: list = []
