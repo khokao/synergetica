9752837b4cd2b825c71f83e1052ea8b98a3156ac
khokao <kokihokao@gmail.com>
2024-07-04 17:21:54 +0900

    minor fix

--- backend/tests/test_generator/test_api/test_main.py
+++ backend/tests/test_generator/test_api/test_main.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from fastapi.testclient import TestClient
 
 from generator.api.main import app
@@ -6,6 +8,6 @@ client = TestClient(app)
 
 
 def test_healthcheck():
-    response = client.get('/healthcheck')
-    assert response.status_code == 200
+    response = client.get('/healthcheck-generator')
+    assert response.status_code == HTTPStatus.OK
     assert response.json() == {'status': 'ok'}
