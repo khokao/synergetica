f214efaa4531766f31b08416b6d61a0e80c50b00
khokao <kokihokao@gmail.com>
2024-07-09 15:58:14 +0900

    apply biome

--- frontend/src/components/FileSidebar/FileSidebar.tsx
+++ frontend/src/components/FileSidebar/FileSidebar.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import { DispDirTree } from "@/components/FileSidebar/LoadFileStructOpen";
+import type React from "react";
 
 export const FileSidebar: React.FC = () => {
   return (
