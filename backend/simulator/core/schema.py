c96db96e02f10a143df4eec511592edace452cdc
Thickstem <mosohaso3560@gmail.com>
2024-08-06 22:05:37 +0900

    clean schema

--- backend/simulator/core/schema.py
+++ backend/simulator/core/schema.py
@@ -1,17 +1,5 @@
 from pydantic import BaseModel
 
-"""
-class GUINode(BaseModel):
-    id: str
-    nodeCategory: str
-    nodeSubcategory: str
-    nodePartsName: str
-    sequence: str
-    controlTo: dict[str, dict[str, str]] | None
-    controlBy: dict[str, dict[str, str]] | None
-    meta: dict[str, float] | None
-"""
-
 
 class GUINode(BaseModel):
     id: str
