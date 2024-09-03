6b11e08fdea07e070a9fa5adb8c3597f0b8cd39a
khokao <kokihokao@gmail.com>
2024-09-03 13:36:02 +0900

    add request cancel middleware

--- backend/generator/api/main.py
+++ backend/generator/api/main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 
 from common.unify_logging_fastapi import setup_logging
 from generator.api.routers import router
+from generator.api.utils import RequestCancelledMiddleware
 
 app = FastAPI()
+app.add_middleware(RequestCancelledMiddleware)
 
 
 @app.get('/healthcheck-generator', include_in_schema=False)
