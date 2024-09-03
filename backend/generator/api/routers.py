e302be310a57c4d71cf83ae27c8aca32be5a0f6e
khokao <kokihokao@gmail.com>
2024-09-03 13:39:40 +0900

    temp update generate rbs api

--- backend/generator/api/routers.py
+++ backend/generator/api/routers.py
@@ -1,6 +1,10 @@
-from fastapi import APIRouter
+import asyncio
+
+from fastapi import APIRouter, HTTPException
+from huggingface_hub import hf_hub_download
 from loguru import logger
 
+from ..core.genetic_algorithm import async_generate_rbs
 from .schemas import GeneratorInput, GeneratorOutput
 
 router = APIRouter()
@@ -9,4 +13,37 @@ router = APIRouter()
 @router.post('/generate', response_model=GeneratorOutput)
 async def generate_sequences(input: GeneratorInput) -> GeneratorOutput:
     logger.info(f'Generating sequences with input: {input.model_dump_json()}')
-    return GeneratorOutput(rbs_sequence='ATGC', promoter_sequence='ATGC')  # Dummy code. (TODO: Replace)
+
+    try:
+        predictor_ckpt_path = await asyncio.to_thread(
+            hf_hub_download,
+            repo_id='gene-circuit-ide/rbs-predictor',
+            filename='v1.ckpt',
+        )
+
+        # Run with dummy inputs.
+        rbs_sequences, rbs_rescaled_predictions = await async_generate_rbs(
+            predictor_ckpt_path=predictor_ckpt_path,
+            target_value=399228.4223,
+            num_iterations=2,
+        )
+        logger.info(f'Generated sequences: {rbs_sequences}')
+        logger.info(f'Generated rescaled predictions: {rbs_rescaled_predictions}')
+        return GeneratorOutput(  # Dummy code. (TODO: Replace)
+            group_node_details={
+                'foobar-group-id-0': [
+                    {'node_category': 'protein', 'sequence': 'ATCG'},
+                ],
+                'foobar-group-id-1': [
+                    {'node_category': 'protein', 'sequence': 'ATCG'},
+                ],
+            }
+        )
+
+    except asyncio.CancelledError as e:
+        logger.error('The task was cancelled due to a client disconnect.')
+        raise HTTPException(status_code=499, detail='Request cancelled') from e
+
+    except Exception as e:
+        logger.error(f'Error during sequence generation: {str(e)}')
+        raise HTTPException(status_code=500, detail='Internal server error') from e
--- backend/generator/api/schemas.py
+++ backend/generator/api/schemas.py
@@ -2,21 +2,22 @@ from pydantic import BaseModel, field_validator
 
 
 class GeneratorInput(BaseModel):
-    rbs_parameter: float
-    rbs_upstream: str
-    rbs_downstream: str
-    promoter_parameter: float
-    promoter_upstream: str
+    reactflow_object_json_str: str
+    rbs_target_parameters: dict[str, float]
 
 
-class GeneratorOutput(BaseModel):
-    rbs_sequence: str
-    promoter_sequence: str
+class ChildNodesDetails(BaseModel):
+    node_category: str  # Used to display the colored sequence in the frontend ?
+    sequence: str
 
-    @field_validator('rbs_sequence', 'promoter_sequence')
+    @field_validator('sequence')
     @classmethod
     def validate_sequence(cls, v: str) -> str:
         valid_nucleotides = {'A', 'T', 'G', 'C'}
         if not all(char in valid_nucleotides for char in v):
             raise ValueError('DNA sequence must contain only A, T, G, and C')
         return v
+
+
+class GeneratorOutput(BaseModel):
+    group_node_details: dict[str, list[ChildNodesDetails]]
