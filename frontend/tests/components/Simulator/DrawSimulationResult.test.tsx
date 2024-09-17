2f3e658bb7c0ae05086ca5af272ef8dbd1bd23aa
Thickstem <mosohaso3560@gmail.com>
2024-09-17 16:27:40 +0900

    mod: simulator front test

--- frontend/tests/components/Simulator/DrawSimulationResult.test.tsx
+++ frontend/tests/components/Simulator/DrawSimulationResult.test.tsx
@@ -18,7 +18,7 @@ global.WebSocket = vi.fn(() => ({
 describe("Graph component", () => {
   const mockConvertResult: ConverterResponseData = {
     num_protein: 2,
-    proteins: ["B3MR1", "AmeR"],
+    proteins: { 'RPp8K6j_urCFeMtsm2pZv': 'BM3R1', 'QaBV3nMXJxcNaNN_hE6ji': 'AmeR' },
     function_str:
       "def ODEtoSolve(var:list[float],t:float,TIR1:float,TIR3:float):\n\td0dt = 300 * 0.5 * ((1.0 + ((1.0-0.2) * 3.0 ** 2.0) / ( var[3] ** 2.0 + 3.0 ** 2.0)) / 1.0) *  15 - 0.012145749 * var[0]\n\td1dt = 1e-05 * TIR1 * var[0] - 0.1 * var[1]\n\td2dt = 300 * 0.5 * ((1.0 + ((1.0-0.2) * 3.0 ** 2.0) / ( var[1] ** 2.0 + 3.0 ** 2.0)) / 1.0) *  15 - 0.012145749 * var[2]\n\td3dt = 1e-05 * TIR3 * var[2] - 0.1 * var[3]\n\treturn (d0dt, d1dt,d2dt, d3dt)",
   };
@@ -28,11 +28,13 @@ describe("Graph component", () => {
   });
 
   it("should initialize WebSocket and display protein parameters when ConvertResult is provided", () => {
-    render(<Graph ConvertResult={mockConvertResult} />);
+    const setSimulatorResult = vi.fn();
+
+    render(<Graph ConvertResult={mockConvertResult} setSimulatorResult={setSimulatorResult} />);
 
     expect(global.WebSocket).toHaveBeenCalledWith("ws://127.0.0.1:8000/ws/simulation");
 
-    for (const protein of mockConvertResult.proteins) {
+    for (const protein of Object.values(mockConvertResult.proteins)) {
       const paramInput = screen.getByText(protein);
       expect(paramInput).toBeInTheDocument();
 
@@ -42,7 +44,9 @@ describe("Graph component", () => {
   });
 
   it("should update protein parameter and send message via WebSocket when slider is changed", () => {
-    render(<Graph ConvertResult={mockConvertResult} />);
+    const setSimulatorResult = vi.fn();
+
+    render(<Graph ConvertResult={mockConvertResult} setSimulatorResult={setSimulatorResult} />);
     const slider = screen.getAllByRole("slider")[0];
 
     fireEvent.change(slider, { target: { value: "10" } });
