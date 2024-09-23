5e34edc459bcb119d034e6ce09faf355de71ef54
Thickstem <mosohaso3560@gmail.com>
2024-09-23 16:31:47 +0900

    mod: front format

--- frontend/tests/components/Simulator/Simulation.test.tsx
+++ frontend/tests/components/Simulator/Simulation.test.tsx
@@ -28,7 +28,7 @@ vi.mock("swr", () => ({
 describe("Simulation Component", () => {
   const mockConvertResult: ConverterResponseData = {
     num_protein: 2,
-    proteins: { 'RPp8K6j_urCFeMtsm2pZv': 'BM3R1', 'QaBV3nMXJxcNaNN_hE6ji': 'AmeR' },
+    proteins: { RPp8K6j_urCFeMtsm2pZv: "BM3R1", QaBV3nMXJxcNaNN_hE6ji: "AmeR" },
     function_str:
       "def ODEtoSolve(var:list[float],t:float,TIR1:float,TIR3:float):\n\td0dt = 300 * 0.5 * ((1.0 + ((1.0-0.2) * 3.0 ** 2.0) / ( var[3] ** 2.0 + 3.0 ** 2.0)) / 1.0) *  15 - 0.012145749 * var[0]\n\td1dt = 1e-05 * TIR1 * var[0] - 0.1 * var[1]\n\td2dt = 300 * 0.5 * ((1.0 + ((1.0-0.2) * 3.0 ** 2.0) / ( var[1] ** 2.0 + 3.0 ** 2.0)) / 1.0) *  15 - 0.012145749 * var[2]\n\td3dt = 1e-05 * TIR3 * var[2] - 0.1 * var[3]\n\treturn (d0dt, d1dt,d2dt, d3dt)",
   };
@@ -36,10 +36,10 @@ describe("Simulation Component", () => {
   const mockReseter = vi.fn();
 
   const mockSimulatorResult: { [key: string]: number } = {
-    'RPp8K6j_urCFeMtsm2pZv': 100,
-    'QaBV3nMXJxcNaNN_hE6ji': 800,
+    RPp8K6j_urCFeMtsm2pZv: 100,
+    QaBV3nMXJxcNaNN_hE6ji: 800,
   };
-  
+
   const mockSetSimulatorResult = vi.fn();
 
   beforeEach(() => {
@@ -49,7 +49,12 @@ describe("Simulation Component", () => {
   it("renders the component correctly", () => {
     render(
       <ReactFlowProvider>
-        <Simulation ConvertResult={mockConvertResult} reseter={mockReseter} SimulatorResult={mockSimulatorResult} setSimulatorResult={mockSetSimulatorResult} />
+        <Simulation
+          ConvertResult={mockConvertResult}
+          reseter={mockReseter}
+          SimulatorResult={mockSimulatorResult}
+          setSimulatorResult={mockSetSimulatorResult}
+        />
       </ReactFlowProvider>,
     );
 
@@ -60,7 +65,12 @@ describe("Simulation Component", () => {
   it("calls reseter function when Reset button is clicked", () => {
     render(
       <ReactFlowProvider>
-        <Simulation ConvertResult={mockConvertResult} reseter={mockReseter} SimulatorResult={mockSimulatorResult} setSimulatorResult={mockSetSimulatorResult} />
+        <Simulation
+          ConvertResult={mockConvertResult}
+          reseter={mockReseter}
+          SimulatorResult={mockSimulatorResult}
+          setSimulatorResult={mockSetSimulatorResult}
+        />
       </ReactFlowProvider>,
     );
 
