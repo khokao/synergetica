e1dc3fedc284b3e193e1b2cc533772a18280758e
ok09ra <okudasota@gmail.com>
2024-10-01 14:32:04 +0900

    fix test

--- backend/tests/test_simulator/test_dynamic_formulation.py
+++ backend/tests/test_simulator/test_dynamic_formulation.py
@@ -24,7 +24,7 @@ def test_mrna_ode_generation(example_data):
     mrna_ode = ode_builder.make_mrna_ode(idx, interact_infos, proteinId_list, all_nodes)
 
     # Assert
-    expected_ode = 'd0dt = 300 * 0.5 * ((0.2 + ((1.0-0.2) * 3.0 ** 2.0) / ( var[3] ** 2.0 + 3.0 ** 2.0)) / 1.0) * 15 - 0.012145749 * var[0]'  # noqa: E501
+    expected_ode = 'd0dt = 0.2 * 0.5 * ((0.2 + ((1.0-0.2) * 3.0 ** 2.0) / ( var[3] ** 2.0 + 3.0 ** 2.0)) / 1.0) * 15 - 0.1 * var[0]'  # noqa: E501
     assert mrna_ode == expected_ode, 'mRNA ODE generation failed.'
 
 
@@ -38,7 +38,7 @@ def test_protein_ode_generation(example_data):
     protein_ode = ode_builder.make_protein_ode(idx, proteinId_list, all_nodes)
 
     # Assert
-    expected_ode = 'd1dt = 0.01 * TIR1 * var[0] - 0.1 * var[1]'
+    expected_ode = 'd1dt = 0.02 * TIR1 * var[0] - 0.1 * var[1]'
     assert protein_ode == expected_ode, 'Protein ODE generation failed.'
 
 
@@ -51,5 +51,5 @@ def test_build_function_as_str(example_data):
     function_str = build_function_as_str(protein_interact_graph, proteinId_list, all_nodes)
 
     # Assert
-    expected_function_str = 'def ODEtoSolve(var:list[float],t:float,TIR1:float,TIR3:float):\n\td0dt = 300 * 0.5 * ((0.2 + ((1.0-0.2) * 3.0 ** 2.0) / ( var[3] ** 2.0 + 3.0 ** 2.0)) / 1.0) * 15 - 0.012145749 * var[0]\n\td1dt = 0.01 * TIR1 * var[0] - 0.1 * var[1]\n\td2dt = 300 * 0.5 * ((0.2 + ((1.0-0.2) * 3.0 ** 2.0) / ( var[1] ** 2.0 + 3.0 ** 2.0)) / 1.0) * 15 - 0.012145749 * var[2]\n\td3dt = 0.01 * TIR3 * var[2] - 0.1 * var[3]\n\treturn (d0dt, d1dt,d2dt, d3dt)'  # noqa: E501
+    expected_function_str = 'def ODEtoSolve(var:list[float],t:float,TIR1:float,TIR3:float):\n\td0dt = 0.2 * 0.5 * ((0.2 + ((1.0-0.2) * 3.0 ** 2.0) / ( var[3] ** 2.0 + 3.0 ** 2.0)) / 1.0) * 15 - 0.1 * var[0]\n\td1dt = 0.02 * TIR1 * var[0] - 0.1 * var[1]\n\td2dt = 0.1 * 0.5 * ((0.2 + ((1.0-0.2) * 3.0 ** 2.0) / ( var[1] ** 2.0 + 3.0 ** 2.0)) / 1.0) * 15 - 0.1 * var[2]\n\td3dt = 0.02 * TIR3 * var[2] - 0.1 * var[3]\n\treturn (d0dt, d1dt,d2dt, d3dt)'  # noqa: E501
     assert function_str == expected_function_str, 'Function string generation failed.'
