00adc1837ca0d19f4a0bf024e6c70cbd5e0f6d7c
khokao <kokihokao@gmail.com>
2024-11-22 19:54:49 +0900

    move tooltip provider to each operator child components

--- frontend/src/components/circuit/operator/operator.tsx
+++ frontend/src/components/circuit/operator/operator.tsx
@@ -1,12 +1,11 @@
 import { ColoredMiniMap } from "@/components/circuit/operator/minimap";
 import { ValidationStatus } from "@/components/circuit/operator/validation-status";
 import { ZoomInOut } from "@/components/circuit/operator/zoom-in-out";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import type { FC } from "react";
 
 export const Operator: FC = () => {
   return (
-    <TooltipProvider>
+    <>
       <ColoredMiniMap />
       <div className="flex items-center mt-1 gap-2 absolute left-4 bottom-4 z-[9]">
         <ZoomInOut />
@@ -14,6 +13,6 @@ export const Operator: FC = () => {
       <div className="flex items-center mt-1 gap-2 absolute left-32 bottom-4 z-[9]">
         <ValidationStatus />
       </div>
-    </TooltipProvider>
+    </>
   );
 };
--- frontend/src/components/circuit/operator/validation-status.tsx
+++ frontend/src/components/circuit/operator/validation-status.tsx
@@ -1,7 +1,7 @@
 import { usePanelContext } from "@/components/circuit/resizable-panel/resizable-panel-context";
 import { useEditorContext } from "@/components/editor/editor-context";
 import { Button } from "@/components/ui/button";
-import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 import * as RadixTooltip from "@radix-ui/react-tooltip";
 import { AlertCircle, CircleCheck } from "lucide-react";
 import type { FC } from "react";
@@ -24,26 +24,29 @@ export const ValidationStatus: FC = () => {
   };
 
   return (
-    <div className="flex bg-gray-100 rounded-xl shadow-lg border border-gray-300" data-testid="validation-status">
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Button
-            variant="ghost"
-            size="icon"
-            className="w-8 h-8 p-0 hover:bg-black/5"
-            onClick={handleClick}
-            aria-label="validation status"
-          >
-            <Icon className={iconClass} />
-          </Button>
-        </TooltipTrigger>
-        {/* Using RadixTooltip.Portal to avoid layout issues caused by parent styles */}
-        <RadixTooltip.Portal>
-          <TooltipContent>
-            <p>Validation status</p>
-          </TooltipContent>
-        </RadixTooltip.Portal>
-      </Tooltip>
-    </div>
+    <TooltipProvider>
+      <div className="flex bg-gray-100 rounded-xl shadow-lg border border-gray-300" data-testid="validation-status">
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              variant="ghost"
+              size="icon"
+              className="w-8 h-8 p-0 hover:bg-black/5"
+              onClick={handleClick}
+              aria-label="validation status"
+              data-testid="validation-status-button"
+            >
+              <Icon className={iconClass} />
+            </Button>
+          </TooltipTrigger>
+          {/* Using RadixTooltip.Portal to avoid layout issues caused by parent styles */}
+          <RadixTooltip.Portal>
+            <TooltipContent>
+              <p>Validation status</p>
+            </TooltipContent>
+          </RadixTooltip.Portal>
+        </Tooltip>
+      </div>
+    </TooltipProvider>
   );
 };
