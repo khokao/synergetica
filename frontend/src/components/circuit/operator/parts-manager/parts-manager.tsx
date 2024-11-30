164567acc84ab427fc0988459978640e41c1f5b0
khokao <kokihokao@gmail.com>
2024-11-30 17:05:48 +0900

    update parts manager

--- frontend/src/components/circuit/operator/parts-manager/parts-manager.tsx
+++ frontend/src/components/circuit/operator/parts-manager/parts-manager.tsx
@@ -1,6 +1,7 @@
 import { PartAddManager } from "@/components/circuit/operator/parts-manager/part-add-manager";
 import { PartDeleteManager } from "@/components/circuit/operator/parts-manager/part-delete-manager";
 import { PartEditManager } from "@/components/circuit/operator/parts-manager/part-edit-manager";
+import { PartsImportExportManager } from "@/components/circuit/operator/parts-manager/parts-import-export-manager";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -12,7 +13,7 @@ export const PartsManager: FC = () => {
     <TooltipProvider>
       <div
         className="flex items-center justify-center bg-gray-100 text-gray-500 rounded-lg shadow-lg border border-gray-300 space-x-2 p-0.5"
-        data-testid=""
+        data-testid="parts-manager"
       >
         <Button variant="ghost" size="icon" className="p-0.5 w-7 h-7 cursor-default">
           <Database className="w-5 h-5 text-gray-500" />
@@ -27,6 +28,10 @@ export const PartsManager: FC = () => {
         <PartDeleteManager />
 
         <PartEditManager />
+
+        <Separator orientation="vertical" className="h-6" />
+
+        <PartsImportExportManager />
       </div>
     </TooltipProvider>
   );
