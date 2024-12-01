4afb6eb2fcf18a0bb2a85e5f0b354de169304d24
khokao <kokihokao@gmail.com>
2024-12-01 17:18:50 +0900

    update node category color settings

--- frontend/src/components/circuit/nodes/parent-node.tsx
+++ frontend/src/components/circuit/nodes/parent-node.tsx
@@ -5,7 +5,7 @@ export const CustomParentNode = ({ id, data }: NodeProps) => {
   const { showParentId } = data;
 
   return (
-    <div className="relative size-full !z-0 bg-gray-100 border-dashed border-2 border-gray-500 rounded-md">
+    <div className="relative size-full !z-0 bg-parent-100 border-parent-500 border-dashed border-2 rounded-md">
       {typeof showParentId === "boolean" && showParentId && (
         <Badge className="absolute top-[-40px] left-0 px-3 py-0 h-8 text-lg">{id}</Badge>
       )}
