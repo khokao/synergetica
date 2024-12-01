a0909a74735acc560691b5743dc87b72735aaa89
khokao <kokihokao@gmail.com>
2024-12-01 17:32:13 +0900

    minor fix to pass biome

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -1,5 +1,5 @@
 "use client";
 
 export const Header = () => {
-  return <header className="flex items-center justify-center py-4 h-16 bg-gray-100"></header>;
+  return <header className="flex items-center justify-center py-4 h-16 bg-gray-100" />;
 };
