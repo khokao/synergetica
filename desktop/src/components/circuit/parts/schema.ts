0628069c6bdea9637eee1d35db5c2949a36d71c6
khokao <kokihokao@gmail.com>
2024-12-23 21:04:39 +0900

    update schema

--- desktop/src/components/circuit/parts/schema.ts
+++ desktop/src/components/circuit/parts/schema.ts
@@ -1,33 +1,57 @@
 import { z } from "zod";
 
-const ControlRelationSchema = z.object({
-  name: z.string().min(1, { message: "Control part name is required." }),
-  type: z.enum(["Activation", "Repression"], { message: "Control type must be 'Activation' or 'Repression'." }),
+const controlByParamsSchema = z.object({
+  Ymax: z.coerce.number().positive({ message: "Ymax must be a positive number." }),
+  Ymin: z.coerce.number().positive({ message: "Ymin must be a positive number." }),
+  K: z.coerce.number().positive({ message: "K must be a positive number." }),
+  n: z.coerce.number().positive({ message: "n must be a positive number." }),
 });
 
-const MetaSchema = z
-  .object({
-    // use the coerce helper: https://github.com/shadcn-ui/ui/issues/421#issuecomment-1561080201
-    Pmax: z.coerce.number({ message: "Pmax must be a number" }).positive({ message: "Pmax must be a positive number" }),
-    Ymax: z.coerce.number({ message: "Ymax must be a number" }).positive({ message: "Ymax must be a positive number" }),
-    Ymin: z.coerce.number({ message: "Ymin must be a number" }).positive({ message: "Ymin must be a positive number" }),
-    K: z.coerce.number({ message: "K must be a number" }).positive({ message: "K must be a positive number" }),
-    n: z.coerce.number({ message: "n must be a number" }).positive({ message: "n must be a positive number" }),
-    Dp: z.coerce.number({ message: "Dp must be a number" }).positive({ message: "Dp must be a positive number" }),
-  })
-  .nullable();
+const controlBySchema = z.object({
+  name: z.string().min(1, { message: "Control part name is required." }),
+  type: z.enum(["Activation", "Repression"], { message: "Control type must be 'Activation' or 'Repression'." }),
+  params: controlByParamsSchema.optional(),
+});
 
-export const partSchema = z.object({
+const basePartSchema = z.object({
   name: z.string().min(1, { message: "Name is required." }),
   description: z.string({ message: "Description must be a string." }),
   category: z.enum(["Promoter", "Protein", "Terminator"], {
     message: "Category must be 'Promoter', 'Protein', or 'Terminator'.",
   }),
   sequence: z.string().regex(/^[ATCGatcg]+$/, { message: "DNA sequence must contain only A, T, C, or G." }),
-  controlBy: z.array(ControlRelationSchema, { message: "controlBy must be an array." }),
-  controlTo: z.array(ControlRelationSchema, { message: "controlTo must be an array." }),
-  meta: MetaSchema,
 });
+
+const promoterSchema = basePartSchema.extend({
+  category: z.literal("Promoter"),
+  controlBy: z.array(controlBySchema),
+  params: z.object({
+    Ydef: z.coerce.number().positive({
+      message: "Ydef must be a positive number for Promoter.",
+    }),
+  }),
+});
+
+const proteinSchema = basePartSchema.extend({
+  category: z.literal("Protein"),
+  controlBy: z.array(controlBySchema).length(0),
+  params: z.object({
+    Dp: z.coerce.number().positive({
+      message: "Dp must be a positive number for Protein.",
+    }),
+    TIRb: z.coerce.number().positive({
+      message: "TIRb must be a positive number for Protein.",
+    }),
+  }),
+});
+
+const terminatorSchema = basePartSchema.extend({
+  category: z.literal("Terminator"),
+  controlBy: z.array(controlBySchema).length(0),
+  params: z.object({}),
+});
+
+export const partSchema = z.discriminatedUnion("category", [promoterSchema, proteinSchema, terminatorSchema]);
 
 export const partsCollectionSchema = z
   .record(z.string(), partSchema)
