9ad125ca49b1c228b9de0387e2c55d38ac141648
khokao <kokihokao@gmail.com>
2025-01-01 02:38:34 +0900

    add new generator api

---
+++ services/src/api/generator/schemas.py
@@ -0,0 +1,14 @@
+from typing import Annotated
+
+from pydantic import BaseModel, Field, PositiveFloat, StringConstraints
+
+DNASequence = Annotated[str, StringConstraints(pattern=r'^[ATGC]+$')]
+
+
+class GeneratorInput(BaseModel):
+    protein_target_values: dict[str, PositiveFloat] = Field(min_length=1)
+    protein_init_sequences: dict[str, DNASequence] = Field(min_length=1)
+
+
+class GeneratorOutput(BaseModel):
+    protein_generated_sequences: dict[str, DNASequence] = Field(min_length=1)
