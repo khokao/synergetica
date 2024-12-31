76aa1b9da5a25da89e796fcc54629388f1bfe889
khokao <kokihokao@gmail.com>
2025-01-01 02:41:52 +0900

    add train test

---
+++ services/tests/test_train/test_scripts/test_train.py
@@ -0,0 +1,32 @@
+import pandas as pd  # Use pandas because huggingface datasets depends on pandas
+from hydra import compose, initialize
+
+from src.train.scripts.train import main
+
+
+def test_train(tmpdir):
+    csv_path = tmpdir / 'test.csv'
+    df = pd.DataFrame(
+        {
+            'sequence': ['ATGC'] * 20,
+            'value': [1.0] * 20,
+        }
+    )
+    df.to_csv(csv_path)
+
+    with initialize(version_base=None, config_path='../../../src/train/configs'):
+        cfg = compose(
+            config_name='default',
+            overrides=[
+                f'trainer.default_root_dir={str(tmpdir)}',
+                f'trainer.logger.save_dir={str(tmpdir)}',
+                'trainer.fast_dev_run=true',
+                f'datamodule.csv_path={str(csv_path)}',
+                'datamodule.sequence_col=sequence',
+                'datamodule.target_col=value',
+                f'datamodule.save_disk_dir={str(tmpdir)}',
+                'datamodule.batch_size=2',
+            ],
+        )
+
+    main(cfg)
