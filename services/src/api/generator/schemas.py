8efac5cc454b04c235dea66bdd3f2f6c2bf75a67
khokao <kokihokao@gmail.com>
2025-01-02 14:08:50 +0900

    accept lowercase sequences

--- services/src/api/generator/schemas.py
+++ services/src/api/generator/schemas.py
@@ -2,7 +2,7 @@ from typing import Annotated
 
 from pydantic import BaseModel, Field, PositiveFloat, StringConstraints
 
-DNASequence = Annotated[str, StringConstraints(pattern=r'^[ATGC]+$')]
+DNASequence = Annotated[str, StringConstraints(pattern=r'^[atgcATGC]+$')]
 
 
 class GeneratorInput(BaseModel):
