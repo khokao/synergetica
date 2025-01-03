db3ec5be8a4bed818a540c72774b26d33f84450a
khokao <kokihokao@gmail.com>
2025-01-04 08:37:45 +0900

    add new simulator codes

---
+++ services/src/api/simulator/constants.py
@@ -0,0 +1,8 @@
+PCN = 15  # Plasmid copy number (unit: copies/cell)
+PCN_REF = 15  # Reference plasmid copy number (unit: copies/cell)
+ZETA = 0.025  # Translate coefficient from RPU to mRNA (unit: [mRNA]/(min·RPU))
+DMRNA = 0.025  # mRNA degradation rate (unit: 1/s)
+ERPU = 0.02  # Translation efficiency coefficient
+
+T0 = 0  # Simulation start time
+TF = 300  # Simulation end time
---
+++ services/src/api/simulator/schemas.py
@@ -0,0 +1,79 @@
+from typing import Literal
+
+from pydantic import BaseModel, Field
+
+
+class ControlByItemParams(BaseModel):
+    Ymin: float
+    Ymax: float
+    K: float
+    n: float
+
+
+class ControlByItem(BaseModel):
+    name: str
+    type: Literal['Repression', 'Activation']
+    params: ControlByItemParams
+
+
+class PromoterParams(BaseModel):
+    Ydef: float
+
+
+class PromoterData(BaseModel):
+    name: str
+    category: Literal['Promoter']
+    sequence: str
+    controlBy: list[ControlByItem]
+    params: PromoterParams
+
+
+class ProteinParams(BaseModel):
+    Dp: float
+    TIRb: float
+
+
+class ProteinData(BaseModel):
+    name: str
+    category: Literal['Protein']
+    sequence: str
+    controlBy: list = Field(max_length=0)
+    params: ProteinParams
+
+
+class TerminatorData(BaseModel):
+    name: str
+    category: Literal['Terminator']
+    sequence: str
+    controlBy: list = Field(max_length=0)
+    params: dict = Field(max_length=0)
+
+
+class ReactFlowParentNode(BaseModel):
+    id: str
+    type: Literal['parent']
+    data: dict = Field(max_length=0)
+
+
+class ReactFlowChildNode(BaseModel):
+    id: str
+    type: Literal['child']
+    data: ProteinData | PromoterData | TerminatorData
+    parentId: str
+
+
+class ReactFlowCustomEdge(BaseModel):
+    type: Literal['custom']
+    source: str
+    target: str
+
+
+class ReactFlowAnnotationEdge(BaseModel):
+    type: Literal['annotation']
+    source: str
+    target: str
+
+
+class ReactFlowObject(BaseModel):
+    nodes: list[ReactFlowParentNode | ReactFlowChildNode]
+    edges: list[ReactFlowCustomEdge | ReactFlowAnnotationEdge]
