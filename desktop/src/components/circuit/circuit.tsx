8d66804fd696d768fc8d57a98496cd864c1e3ade
khokao <kokihokao@gmail.com>
2025-01-21 17:43:38 +0900

    fix zoom range and default zoom

--- desktop/src/components/circuit/circuit.tsx
+++ desktop/src/components/circuit/circuit.tsx
@@ -51,9 +51,9 @@ export const Circuit: React.FC = () => {
         nodeTypes={CircuitNodeTypes}
         edgeTypes={CircuitEdgeTypes}
         connectionMode={ConnectionMode.Loose}
-        defaultViewport={{ x: 100, y: 100, zoom: 1.0 }}
-        minZoom={0.5}
-        maxZoom={1.5}
+        defaultViewport={{ x: 100, y: 100, zoom: 0.5 }}
+        minZoom={0.25}
+        maxZoom={2.0}
         proOptions={{ hideAttribution: true }}
         colorMode="light"
       >
