f93b3028e01b019d0829f09c41d14a921a54c2c7
khokao <kokihokao@gmail.com>
2025-01-25 14:31:52 +0900

    update dnd tests

--- desktop/tests/components/circuit/dnd/dnd-panel.test.tsx
+++ desktop/tests/components/circuit/dnd/dnd-panel.test.tsx
@@ -1,50 +1,47 @@
+import { useDnD } from "@/components/circuit/dnd/dnd-context";
+import { DnDProvider } from "@/components/circuit/dnd/dnd-context";
 import { DnDPanel } from "@/components/circuit/dnd/dnd-panel";
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { describe, expect, it, vi } from "vitest";
+import { describe, expect, it } from "vitest";
 
-const setDnDCategoryMock = vi.fn();
-
-vi.mock("@/components/circuit/dnd/dnd-context", () => ({
-  useDnD: () => [null, setDnDCategoryMock],
-}));
+const TestComponent = () => {
+  const { dndCategory } = useDnD();
+  return <div data-testid="current-category">{dndCategory}</div>;
+};
 
 describe("DnDPanel", () => {
-  beforeEach(() => {
-    vi.useFakeTimers({
-      shouldAdvanceTime: true,
-    });
-  });
-  afterEach(() => {
-    vi.restoreAllMocks();
-  });
-
   it("displays the tooltip when hovering over an icon", async () => {
     // Arrange
-    render(<DnDPanel />);
+    const user = userEvent.setup();
+    render(
+      <DnDProvider>
+        <DnDPanel />
+      </DnDProvider>,
+    );
 
     // Act
-    userEvent.hover(screen.getByTestId("icon-Promoter"));
-    userEvent.hover(screen.getByTestId("icon-Protein"));
-    userEvent.hover(screen.getByTestId("icon-Terminator"));
-    vi.advanceTimersByTime(500);
+    await user.hover(screen.getByTestId("icon-Promoter"));
 
     // Assert
     await waitFor(() => {
       expect(screen.getByRole("tooltip", { name: "Promoter" })).toBeInTheDocument();
-      expect(screen.getByRole("tooltip", { name: "Protein" })).toBeInTheDocument();
-      expect(screen.getByRole("tooltip", { name: "Terminator" })).toBeInTheDocument();
     });
   });
 
   it("calls the onDragStart function with the correct node category when dragging starts", () => {
     // Arrange
-    render(<DnDPanel />);
+    render(
+      <DnDProvider>
+        <DnDPanel />
+        <TestComponent />
+      </DnDProvider>,
+    );
 
     // Act
     fireEvent.dragStart(screen.getByTestId("icon-Promoter"));
 
     // Assert
-    expect(setDnDCategoryMock).toHaveBeenCalledWith("Promoter");
+    expect(screen.getByTestId("current-category")).toHaveTextContent("Promoter");
   });
 });
