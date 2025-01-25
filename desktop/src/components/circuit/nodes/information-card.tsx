3d8849fd346f5e1b0427c70f9cadce929feb50b7
khokao <kokihokao@gmail.com>
2025-01-25 19:40:52 +0900

    refactoring

--- desktop/src/components/circuit/nodes/information-card.tsx
+++ desktop/src/components/circuit/nodes/information-card.tsx
@@ -1,90 +1,57 @@
+import { CATEGORY_CONFIG } from "@/components/circuit/constants";
 import { useParts } from "@/components/circuit/parts/parts-context";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { RiText } from "@remixicon/react";
 import { MoveRight } from "lucide-react";
 
-const CATEGORY_COLORS = {
-  Promoter: "text-promoter-800",
-  Protein: "text-protein-800",
-  Terminator: "text-terminator-800",
-};
-
 const CONTROL_TYPE_ICONS = {
   Repression: <RiText className="rotate-90" data-testid="repression-icon" />,
   Activation: <MoveRight data-testid="activation-icon" />,
 };
 
-const renderControlIcon = (type) => CONTROL_TYPE_ICONS[type];
-
-const renderPartsName = (name) => {
-  const { parts } = useParts();
-
-  const category = parts[name].category;
-  const colorClass = CATEGORY_COLORS[category];
-
-  return <span className={`${colorClass} font-semibold`}>{name}</span>;
-};
-
-const ControlSection = ({ name, category }) => {
-  const { interactionStore } = useParts();
-
-  let controls: Array<{
-    sourceName: string;
-    targetName: string;
-    type: string;
-  }> = [];
-
-  if (category === "Promoter") {
-    const sources = interactionStore.getProteinsByPromoter(name);
-    controls = sources.map((item) => ({
-      sourceName: item.from,
-      targetName: name,
-      type: item.type,
-    }));
-  } else if (category === "Protein") {
-    const targets = interactionStore.getPromotersByProtein(name);
-    controls = targets.map((item) => ({
-      sourceName: name,
-      targetName: item.to,
-      type: item.type,
-    }));
-  }
-
-  if (controls.length === 0) return null;
-
-  return (
-    <div data-testid="information-card-control-section">
-      {controls.map(({ sourceName, targetName, type }) => {
-        return (
-          <Button
-            key={`${sourceName}-${targetName}`}
-            variant="secondary"
-            className="flex items-center justify-center space-x-2 w-[175px] hover:bg-neutral-100/100"
-          >
-            <div className="flex-1 text-center">{renderPartsName(sourceName)}</div>
-            <div className="flex flex-col items-center">{renderControlIcon(type)}</div>
-            <div className="flex-1 text-center">{renderPartsName(targetName)}</div>
-          </Button>
-        );
-      })}
-    </div>
-  );
-};
-
 export const InformationCard = ({ data }) => {
-  const titleColor = CATEGORY_COLORS[data.category] || "";
+  const { name, category, description } = data;
+  const { iconColor } = CATEGORY_CONFIG[category];
+
+  const { parts, interactionStore } = useParts();
+
+  const interactions =
+    category === "Promoter"
+      ? interactionStore.getProteinsByPromoter(name)
+      : category === "Protein"
+        ? interactionStore.getPromotersByProtein(name)
+        : [];
 
   return (
     <Card className="h-full w-full">
       <CardHeader>
-        <CardTitle className={`${titleColor}`} data-testid="information-card-title">
-          {data.name}
+        <CardTitle className={`${iconColor}`} data-testid="information-card-title">
+          {name}
         </CardTitle>
-        <CardDescription data-testid="information-card-description">{data.description}</CardDescription>
+        <CardDescription data-testid="information-card-description">{description}</CardDescription>
       </CardHeader>
-      <CardContent className="flex flex-col justify-center items-center space-y-2">
-        <ControlSection name={data.name} category={data.category} />
+      <CardContent
+        className="flex flex-col justify-center items-center space-y-2"
+        data-testid="information-card-content"
+      >
+        {interactions.map(({ from, to, type }) => {
+          return (
+            <Button
+              key={`${from}-${to}`}
+              variant="secondary"
+              className="flex items-center justify-center space-x-2 w-[175px] hover:bg-neutral-100/100"
+            >
+              <div className={`flex-1 text-center font-semibold ${CATEGORY_CONFIG[parts[from].category].iconColor}`}>
+                {from}
+              </div>
+              <div className="flex flex-col items-center">{CONTROL_TYPE_ICONS[type]}</div>
+              <div className={`flex-1 text-center font-semibold ${CATEGORY_CONFIG[parts[to].category].iconColor}`}>
+                {to}
+              </div>
+            </Button>
+          );
+        })}
       </CardContent>
     </Card>
   );
