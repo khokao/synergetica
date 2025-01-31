f4ea61bdba7486022e1a96398598833e8794ef9a
khokao <kokihokao@gmail.com>
2025-01-31 12:17:36 +0900

    add header test cases

--- desktop/tests/components/header/header.test.tsx
+++ desktop/tests/components/header/header.test.tsx
@@ -1,12 +1,70 @@
 import { Header } from "@/components/header/header";
-import { render, screen } from "@testing-library/react";
-import { describe, expect, it } from "vitest";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { describe, expect, it, vi } from "vitest";
 
-describe("Header Component", () => {
-  it("renders header with Studio and Project navigation items", () => {
+vi.mock("@tauri-apps/plugin-opener", () => {
+  return {
+    openUrl: vi.fn(),
+  };
+});
+
+describe("Header", () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it("calls openUrl with the document URL when the first button is clicked", async () => {
     // Arrange
+    const user = userEvent.setup();
+    const { openUrl } = await import("@tauri-apps/plugin-opener");
     render(<Header />);
 
-    // WIP
+    // Act
+    await user.click(screen.getByTestId("document-button"));
+
+    expect(openUrl).toHaveBeenCalledTimes(1);
+    expect(openUrl).toHaveBeenCalledWith("https://khokao.github.io/synergetica/");
+  });
+
+  it("calls openUrl with the GitHub URL when the first button is clicked", async () => {
+    // Arrange
+    const user = userEvent.setup();
+    const { openUrl } = await import("@tauri-apps/plugin-opener");
+    render(<Header />);
+
+    // Act
+    await user.click(screen.getByTestId("github-button"));
+
+    expect(openUrl).toHaveBeenCalledTimes(1);
+    expect(openUrl).toHaveBeenCalledWith("https://github.com/khokao/synergetica");
+  });
+
+  it("displays document tooltip", async () => {
+    // Arrange
+    const user = userEvent.setup();
+    render(<Header />);
+
+    // Act
+    await user.hover(screen.getByTestId("document-button"));
+
+    // Assert
+    await waitFor(() => {
+      expect(screen.getByRole("tooltip")).toHaveTextContent("Document");
+    });
+  });
+
+  it("displays GitHub tooltip", async () => {
+    // Arrange
+    const user = userEvent.setup();
+    render(<Header />);
+
+    // Act
+    await user.hover(screen.getByTestId("github-button"));
+
+    // Assert
+    await waitFor(() => {
+      expect(screen.getByRole("tooltip")).toHaveTextContent("GitHub");
+    });
   });
 });
